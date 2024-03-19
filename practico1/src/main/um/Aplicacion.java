6b38d965d6553ee3b0b0dc3a9d00b3a68d3cc7b1
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2024-03-19 18:21:02 -0300

    cree la entidad despensa y la probe en el main

--- practico1/src/main/um/Aplicacion.java
+++ practico1/src/main/um/Aplicacion.java
@@ -1,6 +1,25 @@
 package src.main.um;
 
+import src.main.um.entidades.Despensa;
+import src.main.um.entidades.Ingrediente;
+
 public class Aplicacion {
-    public static void main(String[] args) {
-    }
+  public static void main(String[] args) {
+    // Create a new pantry with space for 5 ingredients
+    Despensa despensa = new Despensa(5);
+
+    Ingrediente azucar = new Ingrediente("Azucar", 10);
+    Ingrediente sal = new Ingrediente("Sal", 20);
+    Ingrediente pimienta = new Ingrediente("Pimienta", 30);
+
+    despensa.addIngrediente(azucar);
+    despensa.addIngrediente(sal);
+    despensa.addIngrediente(pimienta);
+
+    System.out.println(despensa.getIngrediente("Azucar", 5));
+    System.out.println(despensa.getIngrediente("Sal", 25));
+    System.out.println(despensa.getIngrediente("Pimienta", 15));
+
+    System.out.println(despensa);
+  }
 }
